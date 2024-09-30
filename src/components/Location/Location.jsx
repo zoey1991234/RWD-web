073d454771cc04a5bcd5af3a2312868f74dfd03a
Zoey <jiaying13111111@gmail.com>
2024-09-30 20:27:19 +0800

    Update Location.jsx

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const Location = () => {
@@ -7,18 +8,18 @@ const Location = () => {
       <section data-aos="fade-up" className="">
         <div className="container my-4">
           <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
-            Location to visit
+            參觀地點
           </h1>
 
-          <div className="rounded-xl ">
+          <div className="rounded-xl">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7116.040113933064!2d83.97168138953894!3d26.90285855661167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39938d631905f7c1%3A0x189dbadf0b42da46!2sNew%20Mobile%20World!5e0!3m2!1sen!2sin!4v1700902186385!5m2!1sen!2sin"
-              width="100%"
-              height="360"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-              style={{ borderRadius: "20px" }}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462784.8949392577!2d121.17672220378604!3d25.016862977571538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ac72bce20a99%3A0x3f6a35cedd0ac2e0!2z6Ie65YyX!5e0!3m2!1szh-TW!2stw!4v1727686099666!5m2!1szh-TW!2stw"
+              width="100%"         
+              height="360"          
+              allowfullscreen=""    
+              loading="lazy"       
+              referrerPolicy="no-referrer-when-downgrade"
+              style={{ borderRadius: "20px", border: "0" }}  
             ></iframe>
           </div>
         </div>
